dc159911283bb2d92f9b7b251e8b48f650419d3d
mayooran <mayooran@hsenidmobile.com>
2013-09-17 14:49:49 +0530

    Added vaadin hello world program

---
+++ src/main/java/com/guru/mayoo/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.guru.mayoo;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+
+public class HelloWorld extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+
+
+    }
+}
